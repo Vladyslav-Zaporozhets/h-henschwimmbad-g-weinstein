7c40daf1fe3d5ebaa09c303c6652094db0df12ea
Vladyslav Zaporozhets <ziper.step@gmail.com>
2025-10-23 19:01:08 +0200

    add some

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,29 @@
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector('[data-menu-open]'),
+    openModalBtn: document.querySelector("[data-menu-open]"),
     // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector('[data-menu-close]'),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
     // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector('[data-menu]'),
+    modal: document.querySelector("[data-menu]"),
+    // Додаємо посилання з мобільного меню
+    menuLinks: document.querySelectorAll(".mob-nav-link"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  // Додаємо обробники подій на кожне посилання в меню
+  refs.menuLinks.forEach((link) => {
+    link.addEventListener("click", toggleModal);
+  });
 
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle('is-open');
+    refs.modal.classList.toggle("is-open");
+
+    // Додамо керування ARIA-атрибутом
+    const isMenuOpen = refs.modal.classList.contains("is-open");
+    refs.openModalBtn.setAttribute("aria-expanded", isMenuOpen);
   }
 })();
